d07e6d7d85b4168da5c9b1f1883fa366b7b02e9f
Даниил <danya.kusaev@mail.ru>
2020-10-20 18:11:30 +0300

    last commit

--- lru_cache.py
+++ lru_cache.py
@@ -1,21 +1,28 @@
 class LRUCache:
-    def __init__(self, capacity: int = 10) -> None:
+    def __init__(self, capacity: int = 5) -> None:
         self.capacity = capacity
         self.cache = {}
+        self.priority = 0
 
     def get(self, key: str) -> str:
         try:
-            return self.cache[key]
+            self.cache[key][1] += 1.01
+            return self.cache[key][0]
         except KeyError:
             return ''
 
     def set(self, key: str, value: str) -> None:
         if len(self.cache) > self.capacity - 1:
-            raise IndexError
-        self.cache[key] = value
+            min_priority = min([v[1] for v in self.cache.values()])
+            del_key = None
+            for k, v in self.cache.items():
+                if v[1] == min_priority:
+                    del_key = k
+                    break
+            self.delete(del_key)
+
+        self.cache[key] = [value, self.priority]
+        self.priority += 1
 
     def delete(self, key: str) -> None:
         del self.cache[key]
-
-
-
--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from my_class import MyList
 a = MyList([1, 2, 3])
 b = MyList([1, 2, 3])
 c = a + b
-print(c)
+print(c)  # вернет [2, 4, 6]
 
 cache = LRUCache(100)
 cache.set('Jesse', 'Pinkman')
@@ -13,3 +13,13 @@ cache.set('Jesse', 'James')
 print(cache.get('Jesse'))  # вернёт 'James'
 cache.delete('Walter')
 print(cache.get('Walter'))  # вернёт ''
+
+# cache.set('1', '1')
+# cache.set('2', '2')
+# cache.set('3', '3')
+# cache.set('4', '4')
+# cache.set('5', '5')
+# print(cache.get('1'))
+# cache.set('6', '6')
+# cache.set('7', '7')
+
